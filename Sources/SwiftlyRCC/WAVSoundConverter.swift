917b6f83e6276264e10f050f8d4a18e1c49b78ec
Ivan Golikov <aeternass@gmail.com>
2019-10-06 17:22:16 +0300

    Added sound converter

---
+++ Sources/SwiftlyRCC/WAVSoundConverter.swift
@@ -0,0 +1,10 @@
+//  Created by Ivan Golikov on 06/10/2019.
+//  Copyright © 2019 Ivan Golikov. All rights reserved.
+
+import Foundation
+
+class WAVSoundConverter: AbstractSoundDataConverter {
+    func convertSound(_ sound: WAVSound) -> Data? {
+        return nil
+    }
+}
